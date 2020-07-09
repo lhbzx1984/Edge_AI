ec1836717b413434c59149fb5275d72e815d0972
lebhoryi <lebhoryi@rt-thread.com>
2020-07-09 11:38:19 +0800

    add tflite model

--- Project1/DNN/STM32CubeIDE/Debug/Application/User/network_data.d
+++
@@ -1,7 +0,0 @@
-Application/User/network_data.o: \
- /home/lebhoryi/RT-Thread/Edge_AI/Project1/DNN/Src/network_data.c \
- ../../Inc/network_data.h ../../Middlewares/ST/AI/Inc/ai_platform.h
-
-../../Inc/network_data.h:
-
-../../Middlewares/ST/AI/Inc/ai_platform.h:
